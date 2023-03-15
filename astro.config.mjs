5c36d31175c7972986d510a4ab22f27d0a0ae2ff
mint <dbell@btr.pm>
2023-03-15 15:10:59 +0800

    add cached images for CI

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,6 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [image({
-    serviceEntryPoint: '@astrojs/image/sharp'
+    serviceEntryPoint: '@astrojs/image/sharp',
   }), astroImageTools, svelte(), mdx()]
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,6 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+// Global type for canvas config located in fluid.js
+const config: {
+  [key: string]: any;
+};
