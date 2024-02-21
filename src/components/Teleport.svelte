9e2d40bb2bf55783e7774ab35336fdc336b78dc8
mint <dashiell.bell@roomle.com>
2024-02-22 01:54:47 +0800

    add more images

---
+++ src/components/Teleport.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+
+    import {onMount} from "svelte";
+
+    export let to: string
+    let teleportWrapper: HTMLElement
+
+    onMount(() => {
+        const toDiv = document.querySelector(to)
+        toDiv.innerHTML = ''
+        const fragment = teleportWrapper.children[0];
+        toDiv.appendChild(fragment)
+    })
+
+</script>
+
+<div class="teleport" bind:this={teleportWrapper}>
+    <slot/>
+</div>
